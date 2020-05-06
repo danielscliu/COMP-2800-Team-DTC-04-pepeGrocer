968355aa95f4c9cc7dceccf240d346088189bdaa
Kevin Shih <shihkster1015@gmail.com>
2020-05-05 19:33:10 -0700

    create sample store list

--- function/addData.js
+++ function/addData.js
@@ -26,13 +26,37 @@ var db = admin.firestore();
 //Write store name data
 //When maps available, dictionary of stores NEED to be long+lat or unique or group search
 
-dictionaryOfStores = ["costco downtown", "Costco Willingdon", "Costco Richmond", "Safeway Robson", "Osaka Supermarket"];
-dictOfAddress = ["605 Expo Blvd, Vancouver, BC V6B 1V4", "4500 Still Creek RD, Burnaby, BC V5C 0B5",
-    "9151 Bridgeport Rd, Richmond, BC V6X 3L9", "1766 Robson St, Vancouver, BC V6G 1E2"]
 
-// function enterMultipleStoreName(item) {
-//     let setDoc = db.collection('stores').doc("costco rmd").set({name: "Costco Richmond"});
-// }
+let storesRef = db.collection("stores");
+
+let set1 = storesRef.doc("costco downtown").set({
+    Name: "Costco Downtown",
+    Address: "605 Expo Blvd, Vancouver, BC V6B 1V4",
+    WaitTime: 5
+});
+let set2 = storesRef.doc("Costco Willingdon").set({
+    Name: "Costco Willingdon",
+    Address: "4500 Still Creek RD, Burnaby, BC V5C 0B5",
+    WaitTime: 5
+});
+let set3 = storesRef.doc("Costco Richmond").set({
+    Name: "Costco Richmond",
+    Address: "9151 Bridgeport Rd, Richmond, BC V6X 3L9",
+    WaitTime: 5
+});
+let set4 = storesRef.doc("Safeway Robson").set({
+    Name: "Safeway Robson",
+    Address: "1766 Robson St, Vancouver, BC V6G 1E2",
+    WaitTime: 5
+});
+let set5 = storesRef.doc("Osaka Supermarket").set({
+    Name: "Osaka Supermarket",
+    Address: "605 Expo Blvd, Vancouver, BC V6B 1V4",
+    WaitTime: 5
+});
+
+let storeList = [set1, set2, set3, set4, set5];
+
 
 
 // let storesRef = db.collection('stores');
