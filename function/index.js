27dcda30414b45d95b20624a72cc2fa9c91f2164
jsui5 <jsui5@my.bcit.ca>
2020-05-06 14:10:07 -0700

    relocated public folder location, added spacing between navitems

--- function/index.js
+++ function/index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("../public"));
 app.set("view engine", "ejs");
 // Firebase init
 
