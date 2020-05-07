a63904dbdc58e9eac1018571254f4f6cdeb00e5f
jsui5 <jsui5@my.bcit.ca>
2020-05-06 17:20:52 -0700

    Fixed external css loading errors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("/public"));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 // Firebase init
 
